5663c1819897eeaf91bb011512478efc3f49edee
Adarsh Singh <adarsh22082012@gmail.com>
2026-02-16 15:58:29 +0530

    review

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,18 @@
+
 function Home() {
   return (
-    <div className="min-h-[70vh] bg-zinc-950 text-zinc-100 flex items-center justify-start pl-15">
-      <div className="text-left">
-        <h1 className="text-4xl font-semibold text-white">
-          Learn Cyber Security From Zero.
-        </h1>
-        <p className="mt-3 text-gray-400 max-w-2xl text-sm">
-          Simple guide for students and everyday users
-        </p>
-      </div>
-    </div>
+    <>
+      <section className="flex flex-col items-center justify-center min-h-[60vh] w-full bg-zinc-950 text-zinc-100 pt-12 pb-6">
+        <div className="flex flex-col items-center justify-center w-full">
+          <h1 className="text-4xl md:text-7xl font-extrabold text-white mb-6 text-center">
+            Learn Cyber Security From Zero.
+          </h1>
+          <p className="mt-2 text-zinc-300 text-2xl text-center max-w-2xl">
+            Simple guide for students and everyday users
+          </p>
+        </div>
+      </section>
+    </>
   );
 }
 
